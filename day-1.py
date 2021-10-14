2ad1fc966338a4765d5a1b1da34056f691271018
umer rukhsar <umerrukhsar65@gmail.com>
2021-10-14 07:12:09 -0700

    updated day-1.py

--- day-1.py
+++ day-1.py
@@ -1,2 +1,21 @@
-# we use double quote with strings for example our name
-print("umer rukhsar")
+# # we use double quote with strings for example our name
+# print("umer rukhsar")
+# # we do not use double / single quote with numbers or variables
+# print(4)
+
+# # we can also perform basic arithematic operations
+# print(2 + 2)
+
+# num = 10
+# print(num)
+# # variable value can be changed
+# num = 1.1
+# print(num)
+
+# # to check a type of variable, we use built-in type function
+# age = 23
+# print(  type(age)  )
+
+# constant can not be changed, they are written in bold letters
+PI = 3.142
+print(2 * PI * 3)
